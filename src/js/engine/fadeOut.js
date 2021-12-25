64b14a2c6f498439083649db9d0cea4ce95a469f
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2021-12-25 23:30:35 +0300

    initial

---
+++ src/js/engine/fadeOut.js
@@ -0,0 +1,15 @@
+export default function fadeOut(fadeTarget) {
+  return new Promise((resolve) => {
+    const fadeEffect = setInterval(() => {
+      if (!fadeTarget.style.opacity) {
+        fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+        fadeTarget.style.opacity -= 0.1;
+      } else {
+        clearInterval(fadeEffect);
+        resolve();
+      }
+    }, 200);
+  });
+}
