145ca6520442a636c30a9466777a5a5f823aa56a
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2022-02-20 20:14:25 +0300

    fixed package.json

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -29,7 +30,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    MiniCssExtractPlugin.loader, 'css-loader', 'style-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader', 
                 ],
             },
             {
